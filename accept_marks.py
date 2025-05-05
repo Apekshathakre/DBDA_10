312dfa8fbd3d9517b5bb6be504503f385c402353
Apekshathakre <94002629+Apekshathakre@users.noreply.github.com>
2025-05-05 13:26:28 +0530

    Add files via upload

---
+++ accept_marks.py
@@ -0,0 +1,11 @@
+marks=int(input("enter the marks"))
+if (marks>70):
+	print("A+")
+elif (60<marks<=70):
+	print("A")
+elif (50<marks<=60):
+	print("B")
+elif (40<marks<=50):
+	print("C")
+else:
+	print("F")
